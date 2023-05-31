7173acbeca2160f7a9c17fab2f7dfebc8cf61892
Ferreira Josué <ferreira.josu@gmail.com>
2023-05-31 15:45:04 +0200

    created SearchBar components and initial fetch api Marseille

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React, { useState } from 'react'
-import MuseumListCard from './components/MuseumListCard'
-import { Button } from 'reactstrap'
+// import { Button } from 'reactstrap'
 import SideBarMuseums from './components/SideBarMuseums'
+import SearchBar from './components/SearchBar'
 
 function App() {
   const [musees, setMusees] = useState()
-  const fetchMusee = async () => {
-    const  returnFetch = await  fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=musees-de-france-base-museofile&q=&rows=10')
-    const fetchjson = await returnFetch.json()
-    setMusees(fetchjson.records)
-    console.log(fetchjson.records)
-  }
+  
   return (
     <div>
+      <SearchBar musees={musees} setMusees={setMusees}/>
       {musees && <SideBarMuseums musees = {musees} />}
-      <Button style={{position: 'fixed', left: '700px'}} onClick={fetchMusee}>salut</Button>
+      {/* <Button style={{position: 'fixed', left: '700px'}} onClick={fetchMusee}>salut</Button> */}
     </div>
     
   )
