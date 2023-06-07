c2293254242646eab9f58406fe195ecb4de01cba
Ferreira Josué <ferreira.josu@gmail.com>
2023-06-07 15:59:12 +0200

    redirect link to museum site error fixed and minor css error fixed

--- client/src/components/MuseumListCard.jsx
+++ client/src/components/MuseumListCard.jsx
@@ -1,76 +1,94 @@
-import React from 'react'
-import {Card, CardBody, CardTitle, CardSubtitle, CardText, Button} from 'reactstrap'
-import styled from 'styled-components'
-import {SiGooglemaps} from 'react-icons/si'
-import {RiSmartphoneFill} from 'react-icons/ri'
-import {BsGlobe} from 'react-icons/bs'
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+} from "reactstrap";
+import styled from "styled-components";
+import { SiGooglemaps } from "react-icons/si";
+import { RiSmartphoneFill } from "react-icons/ri";
+import { BsGlobe } from "react-icons/bs";
 
 const Image = styled.img`
-    max-width: 128px;
-    border-radius: 25px;
-    margin: 20px; 
+  max-width: 128px;
+  border-radius: 25px;
+  margin: 20px;
 `;
 const AdresseBox = styled.div`
-    display: flex;
-    padding-bottom: 10px;
+  display: flex;
+  padding-bottom: 10px;
 `;
 const Adresse = styled.ul`
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
 `;
 const Box = styled.div`
-    display: flex;
-    padding-bottom:'10px';
-    align-items: center;
+  display: flex;
+  padding-bottom: "10px";
+  align-items: center;
 `;
 const Info = styled.div`
-    display: flex;
-    flex-direction: column; 
+  display: flex;
+  flex-direction: column;
 `;
 
-function MuseumListCard({nomMusee, addresse, codePostal, ville, numeroTelphone, site}) {
+function MuseumListCard({
+  nomMusee,
+  addresse,
+  codePostal,
+  ville,
+  numeroTelphone,
+  site,
+}) {
   return (
     <Card
-        style={{
-            marginLeft:'10px',
-            display: 'flex',
-            alignItems: 'flex-start',
-            flexDirection:'row-reverse',
-            width: '30rem',
-            borderBottom: '1px solid lightGrey',
-            paddingBottom: '5px',
-            zIndex: '1000'
-        }}>
-        <Image
-            alt="Sample"
-            src="https://picsum.photos/300/200"
-        />
-        <CardBody style={{width:'100%'}}>
-            <CardTitle tag="h3">
-                {nomMusee}
-            </CardTitle>
-            <Info>
-                <AdresseBox> 
-                    <SiGooglemaps style={{marginRight:'3px'}} />  
-                    <Adresse>
-                        <li>{addresse}</li>
-                        <li>{codePostal}</li>
-                        <li>{ville}</li>
-                    </Adresse>
-                </AdresseBox>
-                <Box>
-                    <RiSmartphoneFill style={{marginRight:'3px'}}/>  
-                    {numeroTelphone}
-                </Box>
-                <Box>
-                    <BsGlobe style={{marginRight:'5px'}}/>
-                    <a href={site} target="_blank" style={{color:'black' }}>{site.split('/')[0]}</a> 
-                </Box>
-            </Info>
-        </CardBody>
-  </Card>
-  )
+      style={{
+        marginLeft: "10px",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "row-reverse",
+        width: "30rem",
+        borderBottom: "1px solid lightGrey",
+        paddingBottom: "5px",
+        zIndex: "1000",
+      }}
+    >
+      <Image alt="Sample" src="https://picsum.photos/300/200" />
+      <CardBody style={{ width: "100%" }}>
+        <CardTitle tag="h3">
+          {nomMusee.charAt(0).toUpperCase() + nomMusee.slice(1)}
+        </CardTitle>
+        <Info>
+          <AdresseBox>
+            <SiGooglemaps style={{ marginRight: "3px" }} />
+            <Adresse>
+              <li>{addresse}</li>
+              <li>{codePostal}</li>
+              <li>{ville}</li>
+            </Adresse>
+          </AdresseBox>
+          <Box>
+            <RiSmartphoneFill style={{ marginRight: "3px" }} />
+            {numeroTelphone}
+          </Box>
+          <Box>
+            <BsGlobe style={{ marginRight: "5px" }} />
+            <a
+              href={"https://" + site}
+              target="_blank"
+              style={{ color: "black" }}
+            >
+              {site.split("/")[0]}
+            </a>
+          </Box>
+        </Info>
+      </CardBody>
+    </Card>
+  );
 }
 
-export default MuseumListCard
+export default MuseumListCard;
